bf6e1779996af82e5c4201e25664a31ea5c8dfcc
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-14 12:12:34 +0530

    Group Routing

---
+++ level3/src/app/dashboard/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        dashboard page
+      </h1>
+    </div>
+  )
+}
+
+export default page
