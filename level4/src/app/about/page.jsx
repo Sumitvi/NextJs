c68b88f8953096782bbfd15ddc351fa66cee4a84
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-14 14:06:10 +0530

    Fonts

--- level4/src/app/about/page.jsx
+++ level4/src/app/about/page.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Image from 'next/image'
 
 const page = () => {
   return (
     <div>
       <h1>About Page</h1>
+
+      <Image src="/vercel.svg" alt="About Image" width={200} height={200}
+    
+      />
     </div>
   )
 }
