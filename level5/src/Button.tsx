3b154f03c3e01421dc9084043bd195cc0133f1bc
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-19 13:26:09 +0530

    Level5

---
+++ level5/src/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+      <button className='bg-blue-500 hover:bg-blue-950 text-yellow-300'>Click me</button>
+    </div>
+  )
+}
+
+export default Button
--- level5/src/types.d.ts
+++ level5/src/types.d.ts
@@ -1,3 +1,5 @@
+// used for global types accessible across the project without importing
+
 declare global{
     type sumit = {
         name : string
