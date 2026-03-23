0289cb5050a0fb38796b4c25a03bf8208543c2d6
Sumit Vishwakarma <sv0042595@gmail.com>
2026-03-23 13:23:13 +0530

    Apis

--- level6/src/app/api/user/route.ts
+++ level6/src/app/api/user/route.ts
@@ -1,9 +1,22 @@
 import { NextResponse } from "next/server";
 
+
+// Get Request Handler
 export async function GET() {
  return NextResponse.json({
     name: "John Doe",
     email: "johndoe@gmail.com",
     age: 30,
  })
+}
+
+// Post Request Handler
+export async function POST(request: Request) {
+    let { name, email, age } = await request.json();
+
+    return NextResponse.json({
+        name,
+        email,
+        age,
+    })
 }
